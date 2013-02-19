54001ea947416e97d14cc2065088b8ec761ba336
goldquest <tjwelde@gmail.com>
2013-02-19 21:14:37 +0100

    new php client version

--- etherpad-lite-client.php
+++ etherpad-lite-client.php
@@ -14,7 +14,7 @@
 
 class EtherpadLiteClient {
 
-  const API_VERSION             = 1;
+  const API_VERSION             = '1.1';
 
   const CODE_OK                 = 0;
   const CODE_INVALID_PARAMETERS = 1;
@@ -26,7 +26,11 @@ class EtherpadLiteClient {
   protected $baseUrl = "http://localhost:9001/api";
   
   public function __construct($apiKey, $baseUrl = null){
+    if (strlen($apiKey) < 1){
+      throw new InvalidArgumentException("[{$apiKey}] is not a valid API key");
+    }
     $this->apiKey  = $apiKey;
+
     if (isset($baseUrl)){
       $this->baseUrl = $baseUrl;
     }
@@ -35,37 +39,59 @@ class EtherpadLiteClient {
     }
   }
 
-  protected function call($function, array $arguments = array()){
-    $query = array_merge(
-      array('apikey' => $this->apiKey),
-      $arguments
-    ); 
-    $url = $this->baseUrl."/".self::API_VERSION."/".$function."?".http_build_query($query);
-    // not all PHP installs have access to curl
+  protected function get($function, array $arguments = array()){
+    return $this->call($function, $arguments, 'GET');
+  }
+
+  protected function post($function, array $arguments = array()){
+    return $this->call($function, $arguments, 'POST');
+  }
+
+  protected function call($function, array $arguments = array(), $method = 'GET'){
+    $arguments['apikey'] = $this->apiKey;
+    $arguments = http_build_query($arguments, '', '&');
+    $url = $this->baseUrl."/".self::API_VERSION."/".$function;
+    if ($method !== 'POST'){
+      $url .=  "?".$arguments;
+    }
+    // use curl of it's available
     if (function_exists('curl_init')){
       $c = curl_init($url);
       curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($c, CURLOPT_TIMEOUT, 20);
+      if ($method === 'POST'){
+        curl_setopt($c, CURLOPT_POST, true);
+        curl_setopt($c, CURLOPT_POSTFIELDS, $arguments);
+      }
+      
+      // CODE_CHANGE_TW SSL Support
       global $CFG;
       if($CFG->etherpadlite_check_ssl) {
-      	// CODE_CHANGE_TW 09.12.2011 - Next is the proper solution
-      	curl_setopt($c, CURLOPT_SSL_VERIFYPEER, true);
-      	curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 2);
-      	// CODE_CHANGE_TW end
+          // CODE_CHANGE_TW 09.12.2011 - Next is the proper solution
+          curl_setopt($c, CURLOPT_SSL_VERIFYPEER, true);
+          curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 2);
+          // CODE_CHANGE_TW end
       }
       else {
           // CODE_CHANGE_TW 07.12.2011 - Because curl doesn't trust any certificate, I had to add this little "hack" here to enable https support
           curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
       }
-      $result = curl_exec($c);
-
-      curl_close($c);
+      // CODE_CHANGE_TW end
       
+      $result = curl_exec($c);
+      curl_close($c);
+    // fallback to plain php
     } else {
-      $result = file_get_contents($url);
+      $params = array('http' => array('method' => $method, 'ignore_errors' => true, 'header' => 'Content-Type:application/x-www-form-urlencoded'));
+      if ($method === 'POST'){
+        $params['http']['content'] = $arguments;
+      }
+      $context = stream_context_create($params);
+      $fp = fopen($url, 'rb', false, $context);
+      $result = $fp ? stream_get_contents($fp) : null;
     }
     
-    if($result == "" || !$result){
+    if(!$result){
       throw new UnexpectedValueException("Empty or No Response from the server");
     }
     
@@ -107,37 +133,42 @@ class EtherpadLiteClient {
   
   // creates a new group 
   public function createGroup(){
-    return $this->call("createGroup");
+    return $this->post("createGroup");
   }
 
   // this functions helps you to map your application group ids to etherpad lite group ids 
   public function createGroupIfNotExistsFor($groupMapper){
-    return $this->call("createGroupIfNotExistsFor", array(
+    return $this->post("createGroupIfNotExistsFor", array(
       "groupMapper" => $groupMapper
     ));
   }
 
   // deletes a group 
   public function deleteGroup($groupID){
-    return $this->call("deleteGroup", array(
+    return $this->post("deleteGroup", array(
       "groupID" => $groupID
     ));
   }
 
   // returns all pads of this group
   public function listPads($groupID){
-    return $this->call("listPads", array(
+    return $this->get("listPads", array(
       "groupID" => $groupID
     ));
   }
 
   // creates a new pad in this group 
-  public function createGroupPad($groupID, $padName, $text=null){
-    return $this->call("createGroupPad", array(
+  public function createGroupPad($groupID, $padName, $text){
+    return $this->post("createGroupPad", array(
       "groupID" => $groupID,
       "padName" => $padName,
       "text"    => $text
     ));
+  }
+
+  // list all groups
+  public function listAllGroups(){
+    return $this->get("listAllGroups");
   }
 
   // AUTHORS
@@ -145,16 +176,30 @@ class EtherpadLiteClient {
 
   // creates a new author 
   public function createAuthor($name){
-    return $this->call("createAuthor", array(
+    return $this->post("createAuthor", array(
       "name" => $name
     ));
   }
 
   // this functions helps you to map your application author ids to etherpad lite author ids 
   public function createAuthorIfNotExistsFor($authorMapper, $name){
-    return $this->call("createAuthorIfNotExistsFor", array(
+    return $this->post("createAuthorIfNotExistsFor", array(
       "authorMapper" => $authorMapper,
       "name"         => $name
+    ));
+  }
+
+  // returns the ids of all pads this author as edited
+  public function listPadsOfAuthor($authorID){
+    return $this->get("listPadsOfAuthor", array(
+      "authorID" => $authorID
+    ));
+  }
+
+  // Gets an author's name
+  public function getAuthorName($authorID){
+    return $this->get("getAuthorName", array(
+      "authorID" => $authorID
     ));
   }
 
@@ -165,7 +210,7 @@ class EtherpadLiteClient {
 
   // creates a new session 
   public function createSession($groupID, $authorID, $validUntil){
-    return $this->call("createSession", array(
+    return $this->post("createSession", array(
       "groupID"    => $groupID,
       "authorID"   => $authorID,
       "validUntil" => $validUntil
@@ -174,28 +219,28 @@ class EtherpadLiteClient {
 
   // deletes a session 
   public function deleteSession($sessionID){
-    return $this->call("deleteSession", array(
+    return $this->post("deleteSession", array(
       "sessionID" => $sessionID
     ));
   }
 
   // returns informations about a session 
   public function getSessionInfo($sessionID){
-    return $this->call("getSessionInfo", array(
+    return $this->get("getSessionInfo", array(
       "sessionID" => $sessionID
     ));
   }
 
   // returns all sessions of a group 
   public function listSessionsOfGroup($groupID){
-    return $this->call("listSessionsOfGroup", array(
+    return $this->get("listSessionsOfGroup", array(
       "groupID" => $groupID
     ));
   }
 
   // returns all sessions of an author 
   public function listSessionsOfAuthor($authorID){
-    return $this->call("listSessionsOfAuthor", array(
+    return $this->get("listSessionsOfAuthor", array(
       "authorID" => $authorID
     ));
   }
@@ -204,18 +249,36 @@ class EtherpadLiteClient {
   // Pad content can be updated and retrieved through the API
 
   // returns the text of a pad 
-  // should take optional $rev
-  public function getText($padID){
-    return $this->call("getText", array(
-      "padID" => $padID
-    ));
+  public function getText($padID, $rev=null){
+    $params = array("padID" => $padID);
+    if (isset($rev)){
+      $params["rev"] = $rev;
+    }
+    return $this->get("getText", $params);
+  }
+
+  // returns the text of a pad as html
+  public function getHTML($padID, $rev=null){
+    $params = array("padID" => $padID);
+    if (isset($rev)){
+      $params["rev"] = $rev;
+    }
+    return $this->get("getHTML", $params);
   }
 
   // sets the text of a pad 
   public function setText($padID, $text){
-    return $this->call("setText", array(
+    return $this->post("setText", array(
       "padID" => $padID, 
       "text"  => $text
+    ));
+  }
+
+  // sets the html text of a pad 
+  public function setHTML($padID, $html){
+    return $this->post("setHTML", array(
+      "padID" => $padID, 
+      "html"  => $html
     ));
   }
 
@@ -226,36 +289,60 @@ class EtherpadLiteClient {
 
   // creates a new pad
   public function createPad($padID, $text){
-    return $this->call("createPad", array(
+    return $this->post("createPad", array(
       "padID" => $padID, 
       "text"  => $text
-    ));
+    ), 'POST');
   }
 
   // returns the number of revisions of this pad 
   public function getRevisionsCount($padID){
-    return $this->call("getRevisionsCount", array(
+    return $this->get("getRevisionsCount", array(
+      "padID" => $padID
+    ));
+  }
+
+  // returns the number of users currently editing this pad
+  public function padUsersCount($padID){
+    return $this->get("padUsersCount", array(
+      "padID" => $padID
+    ));
+  }
+
+  // return the time the pad was last edited as a Unix timestamp
+  public function getLastEdited($padID){
+    return $this->get("getLastEdited", array(
       "padID" => $padID
     ));
   }
 
   // deletes a pad 
   public function deletePad($padID){
-    return $this->call("deletePad", array(
+    return $this->post("deletePad", array(
       "padID" => $padID
     ));
   }
 
   // returns the read only link of a pad 
   public function getReadOnlyID($padID){
-    return $this->call("getReadOnlyID", array(
+    return $this->get("getReadOnlyID", array(
+      "padID" => $padID
+    ));
+  }
+
+  // returns the ids of all authors who've edited this pad
+  public function listAuthorsOfPad($padID){
+    return $this->get("listAuthorsOfPad", array(
       "padID" => $padID
     ));
   }
 
   // sets a boolean for the public status of a pad 
   public function setPublicStatus($padID, $publicStatus){
-    return $this->call("setPublicStatus", array(
+    if (is_bool($publicStatus)) {
+      $publicStatus = $publicStatus ? "true" : "false";
+    }
+    return $this->post("setPublicStatus", array(
       "padID"        => $padID,
       "publicStatus" => $publicStatus
     ));
@@ -263,14 +350,14 @@ class EtherpadLiteClient {
 
   // return true of false 
   public function getPublicStatus($padID){
-    return $this->call("getPublicStatus", array(
+    return $this->get("getPublicStatus", array(
       "padID" => $padID
     ));
   }
 
   // returns ok or a error message 
   public function setPassword($padID, $password){
-    return $this->call("setPassword", array(
+    return $this->post("setPassword", array(
       "padID"    => $padID,
       "password" => $password
     ));
@@ -278,8 +365,23 @@ class EtherpadLiteClient {
 
   // returns true or false 
   public function isPasswordProtected($padID){
-    return $this->call("isPasswordProtected", array(
+    return $this->get("isPasswordProtected", array(
       "padID" => $padID
+    ));
+  }
+
+  // Get pad users
+  public function padUsers($padID){
+    return $this->get("padUsers", array(
+      "padID" => $padID
+    ));
+  }
+
+  // Send all clients a message
+  public function sendClientsMessage($padID, $msg){
+    return $this->post("sendClientsMessage", array(
+      "padID" => $padID,
+      "msg"   => $msg
     ));
   }
 }
