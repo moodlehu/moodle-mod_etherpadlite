53a311f8cbe8a398773f4f91b3beae5db1ea92b2
goldquest <tjwelde@gmail.com>
2014-05-13 16:33:41 +0200

    Added missing GPL copyright statements and modified docblocks -> removed subpackage and made package FRANKENSTYLE

--- lib.php
+++ lib.php
@@ -1,4 +1,18 @@
-<?php  // $Id: lib.php,v 1.7.2.5 2009/04/22 21:30:57 skodak Exp $
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Library of functions and constants for module etherpadlite
@@ -14,8 +28,7 @@
  *     help to save some memory when Moodle is performing
  *     actions across all modules.
  *
- * @package    mod
- * @subpackage etherpadlite
+ * @package    mod_etherpadlite
  *
  * @author     Timo Welde <tjwelde@gmail.com>
  * @copyright  2012 Humboldt-Universität zu Berlin <moodle-support@cms.hu-berlin.de>
--- mod_form.php
+++ mod_form.php
@@ -1,4 +1,18 @@
-<?php //$Id: mod_form.php,v 1.2.2.3 2009/03/19 12:23:11 mudrd8mz Exp $
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * This file defines the main etherpadlite configuration form
@@ -23,8 +37,7 @@
  *             See lib/weblib.php Constants and the format_text()
  *             function for more info
  *
- * @package    mod
- * @subpackage etherpadlite
+ * @package    mod_etherpadlite
  *
  * @author     Timo Welde <tjwelde@gmail.com>
  * @copyright  2012 Humboldt-Universität zu Berlin <moodle-support@cms.hu-berlin.de>
