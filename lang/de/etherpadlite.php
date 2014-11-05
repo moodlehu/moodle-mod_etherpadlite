9a2473e3f164078852f12f02aeeceda4863bb452
Dmytro Vorona <alendit@gmail.com>
2014-11-05 13:15:02 +0100

    Expanded German translation in user view

--- lang/de/etherpadlite.php
+++ lang/de/etherpadlite.php
@@ -29,8 +29,13 @@ $string['modulename_help'] = 'Das Etherpad Lite modul erlaubt es Studenten und L
 
 (Der Etherpad-Server, welcher hinter diesem Modul steht, ist noch im beta Stadium. Deshalb können Probleme auftreten, was allerdings nicht erwartet wird)';
 
+$string['etherpadlitename'] = 'Pad-Name';
+
 $string['etherpadlite:addinstance'] = 'Neues Pad hinzufügen';
+$string['guestsallowed'] = 'Dürfen Gäste das Pad ändern?';
+$string['guestsallowed_help'] = 'Diese Einstellung bestimmt, ob die Gäste das Pad beschreiben können. Ansonsten ist ihnen nur ein Lesezugriff gewährt.';
 
 // view
 $string['summaryguest'] = 'Sie sind als Gast eingeloggt, deshalb sehen Sie nur den reinen Text dieses Pads. Um neue &Auml;nderungen zu sehen, laden Sie diese Seite neu.';
 ?>
+
