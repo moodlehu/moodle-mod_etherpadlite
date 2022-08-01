c076556924925cddff914ee4cdf630907751a904
Adrian Czermak <adrian.czermak@univie.ac.at>
2022-08-01 18:25:09 +0200

    readonly url and groupmode fix
    commit acd9bc36ec310cdfdc7bf94e5426a7da8cb3fc0d
    Author: Adrian Czermak <adrian.czermak@univie.ac.at>
    Date:   Mon Aug 1 18:23:26 2022 +0200

--- lib.php
+++ lib.php
@@ -360,7 +360,7 @@ function etherpadlite_gen_random_string() {
     $characters = "0123456789";
     $string = "";
     for ($p = 0; $p < $length; $p++) {
-        $string .= $characters[mt_rand(0, strlen($characters))];
+        $string .= $characters[mt_rand(0, strlen($characters) - 1)];
     }
     return $string;
 }
